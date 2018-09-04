549eb8a825549fa80f030f9f8f617fbe173be4da
bobwen <741147238@qq.com>
2018-09-04 17:38:52 +0800

    Initial Commit

---
+++ MyTabletablew/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MyTabletablew
+//
+//  Created by bobwen on 2018/9/4.
+//  Copyright © 2018 bobwen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
